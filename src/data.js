7e98e329afb41bd6f635217aa17433d0d1981149
Peter Alexander <renormalize@gmail.com>
2021-09-10 16:18:45 -0700

    Add demo username and pass

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const data = {
     projects: [
         {
             title: "Thought Museum",
-            summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            summary: "Username: teacher Password: ReeMSxcw63 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
             visitSite: "https://thoughtmuseum-app.azurewebsites.net",
             repos: [
                 {
